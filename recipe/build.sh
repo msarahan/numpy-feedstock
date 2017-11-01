a40d4c9c815efc0265547e579a88dedbfb384514
Jonathan Helmus <jjhelmus@gmail.com>
2017-11-01 14:42:49 -0500

    do not use fortran when building openblas variant
    A fortran compiler is not strickly required when building NumPy.  If built
    without Fortran LDFLAGS, does not need to be modified.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,6 @@ if [ "$blas_impl" = "mkl" ]; then
 printf "\n\n__mkl_version__ = \"$mkl\"\n" >> numpy/__init__.py
 fi
 
-if [ "$blas_impl" = "openblas" ] && [ $(uname) == Linux ]; then
-    # add -shared to LDFLAGS to prevent linking errors similar to:
-    # In function `_start':
-    # (.text+0x20): undefined reference to `main'
-    # these seems to only occur when using gfortran
-    export LDFLAGS="${LDFLAGS} -shared"
-fi
-
 # site.cfg comes from blas devel package (e.g. mkl-devel)
 cp $PREFIX/site.cfg site.cfg
 
